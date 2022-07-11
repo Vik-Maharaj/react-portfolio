0fc3cd7856be822a594bf4f2db48cfaad8c1545d
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-11 16:27:56 -0400

    Update main app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Main from "./views/Main";
 import Projects from "./views/Projects";
@@ -9,20 +9,13 @@ import Contact from "./views/Contact";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="">
+            <Main />
+            <Projects />
+            <About />
+            <Resume />
+            <Contact />
+          </div> 
     </div>
   );
 }
