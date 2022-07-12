2b6e0e9332b9ec9f7344fcbd0e016a8c902b2521
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-12 14:49:19 -0400

    Update nav component

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,4 +2,19 @@ import React from "react";
 import "./style.css";
 
 
+function NavBar() {
+    return (
+      <nav className="navbar navbar-expand-md">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavMenu">
+          <i class="fas fa-bars fa-lg"></i>
+        </button>
+
+      </nav>
+    );
+  }
+
 export default NavBar;
