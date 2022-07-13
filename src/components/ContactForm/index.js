8b47ea905c92c3ad7051e308250e7c7717677cb7
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-13 18:49:41 -0400

    Update contact form component

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 
+function ContactForm() {
+
+    return (
+        <div className="myForm">
+            <form id="email-form" method="POST">
+                <div className="form-group">
+                    <label for="form-name">Name</label>
+                    <input type="text" name="name" className="form-control" id="form-name"  placeholder="Your Name"></input>
+                </div>
+        </div>
+    )
+}
 
 export default ContactForm;
 
