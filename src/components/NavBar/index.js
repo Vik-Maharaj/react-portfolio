f58af3c3257440ec404ab0bd889a92b26b237c25
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-13 18:34:06 -0400

    Update nav component index.js

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -9,10 +9,41 @@ function NavBar() {
           className="navbar-toggler"
           type="button"
           data-toggle="collapse"
-          data-target="#navbarNavMenu">
+          data-target="#navbarNavMenu"
+          aria-controls="navbarNavMenu"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <i class="fas fa-bars fa-lg"></i>
         </button>
-
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="navbarNavMenu"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item navbar-text">
+              <Link
+                to="projects"
+                className="nav-link"
+                activeClass="active"
+                spy={true}
+                smooth={true}
+              >
+                PROJECTS
+              </Link>
+            </li>
+            <li className="nav-item navbar-text">
+              <Link
+                to="about"
+                className="nav-link"
+                activeClass="active"
+                spy={true}
+                smooth={true}
+              >
+                ABOUT
+              </Link>
+          </ul>
+        </div>
       </nav>
     );
   }
