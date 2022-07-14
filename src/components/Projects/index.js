65deba7c622b451487acb4ee95cd66157ed204dc
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-14 12:14:36 -0400

    Update project component

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,6 +1,28 @@
 import React from "react";
 import projects from "../../projects";
 
+function Project() {
+    return projects.map(project => {
+        if (projects.indexOf(project) === 0 || projects.indexOf(project) %2 === 0 ) {
+            return (
+                <div className="row projectRow">
+                    <div className="col-md-4">
+                        <img src={project.image} alt={project.title} width="200px"></img>
+                    </div>
+                    <div className="col-md-8">
+                        <h4 className="projectTitle">{project.title}</h4>
+                        <p className="projectAbout">{project.about}</p>
+                        <ul className="technologies">
+                            <Technologies techItems={project.technologies}></Technologies>
+                        </ul>
+                        <div className="row">
+                            <button type="button" className="btn btn-info"><a className="repoLink" href={project.repo}>Repo</a></button>
+                        </div>
+                    </div>
+                 
+                </div>
+            )
+}
 export default Project;
 
 
