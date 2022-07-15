192f9c2e3b8e4f33183296730380bd4ed29e5428
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-15 13:33:58 -0400

    Update main views index.js

--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -6,8 +6,33 @@ function MainPage() {
       <div className="row" id="/">
         <NavBar />
   
+        <div className="col-md">
+          <div className="mobile-image"></div>
+          <div className="intro-text alignCenter">
+            <h2 className="mobile-title">Vik Maharaj</h2>
+            <br></br>
+            <h2 className="intro">Full Stack Web Developer</h2>
+            <br></br>
+            <h3>Developer with a passion for UX/UI and professional experience in graphic design and digital media production.</h3>
+            <br></br>
+            <br></br>
+            <Link to="about"
+             activeClass="active"
+             spy={true}
+             smooth={true}
+            >
+              <button type="button" className="readMore btn btn-info">
+                Read More
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="col-md">
+          <div className="right-image"></div>
+        </div>
+      </div>
     );
   }
 
-
+  
 export default MainPage;
