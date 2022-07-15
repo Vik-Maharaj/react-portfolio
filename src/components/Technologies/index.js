c49344fdd83d6809e00843506a1571be0ce4d0e0
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-15 09:50:56 -0400

    Update technologies component

--- src/components/Technologies/index.js
+++ src/components/Technologies/index.js
@@ -1,4 +1,10 @@
 import React from "react";
 
+function Technologies(props) {
+
+    return props.technoliges.map(technology => {
+            return <li className="techUsed">{technology}</li>
+    })
+}
 
 export default Technologies;
