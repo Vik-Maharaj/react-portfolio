4018f153575c37b3d7e3bd8d256bbd30ff77d2f1
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-15 14:01:43 -0400

    Update index.js for about views

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -5,7 +5,28 @@ function About() {
 
     return (
         <div className="viewThree" id="about">
-
+            <div className="row">
+                <div className="col-md about-left">
+                    <img src={MeOnWater} className="about-photo img-fluid" alt="Me"></img>
+                </div>
+                <div className="col-md">
+                    <h3 className="viewTitle" id="aboutTitle">ABOUT</h3>
+                    <div className="row align-self-center bioText">
+                        <p className="bio">Here's some more about me:</p>
+                        <p className="bio"><span className="skillHeader">Technical Skills:</span>
+                            <ul className=" skillList text-center">
+                                <li className="skill">HTML</li>
+                                <li className="skill">CSS</li>
+                                <li className="skill">JavaScript</li>
+                                <li className="skill">Node.js</li>
+                                <li className="skill">React.js</li>
+                                <li className="skill">Express.js</li>
+                            </ul>
+                        </p>
+                        <p className="bio">Hit me up!</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
