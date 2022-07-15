99e8617344fa46d1351f8916eccf3e69a22c7103
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-15 11:49:45 -0400

    Update main index.js views

--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import NavBar from "../../components/NavBar";
 
+function MainPage() {
+    return (
+      <div className="row" id="/">
+        <NavBar />
+  
+    );
+  }
+
 
 export default MainPage;
