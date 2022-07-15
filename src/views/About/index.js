e2794cc79389c51e86a809a0ce48dd47a9f5a596
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-15 13:48:15 -0400

    Update index.js for about views

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -1,4 +1,13 @@
 import React from "react";
 import "./style.css";
 
+function About() {
+
+    return (
+        <div className="viewThree" id="about">
+
+        </div>
+    )
+}
+
 export default About;
