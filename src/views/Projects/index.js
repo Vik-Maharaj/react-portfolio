066887df91e30d2e4faf88195697fd70e4dc04cc
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-22 21:04:36 -0400

    Update projects function

--- src/views/Projects/index.js
+++ src/views/Projects/index.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./style.css";
 
 function Projects() {
-
+    return (
+        <div className="viewTwo" id="projects">
+            <div className="row">
+                <div className="col-md">
+                    <h3 className="viewTitle" id="projectsTitle">SELECTED PROJECTS</h3>
+                </div>
+            </div>
+            <div className="row">
+                <Project2 />
+            </div>
+        </div>
+    )
 }
 
 export default Projects;
