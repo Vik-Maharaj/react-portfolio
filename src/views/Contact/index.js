9231f0a1158952ec6cc1768e82e363bf439f369c
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-22 20:50:32 -0400

    Update contact function

--- src/views/Contact/index.js
+++ src/views/Contact/index.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./style.css";
 
 function Contact() {
-
+    return (
+        <div className="viewFive" id="contact">
+          <div className="row contact-row">
+            <div className="formDiv" id="contact">
+              <h3 className="viewTitle" id="contactTitle">
+                HEY THERE, LET'S CONNECT
+              </h3>
+    
+              <hr></hr>
+    
+              <SocialLinks />
+            </div>
+          </div>
+        </div>
+      );
 
 }
 
