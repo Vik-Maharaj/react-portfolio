06556801269426741a5eddde5c1050cc20cacf29
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-27 09:55:23 -0400

    Update contact page index

--- src/views/Contact/index.js
+++ src/views/Contact/index.js
@@ -1,23 +1,25 @@
 import React from "react";
 import "./style.css";
+import ContactForm from "../../components/ContactForm";
+import SocialMedia from "../../components/SocialMedia";
 
 function Contact() {
-    return (
-        <div className="viewFive" id="contact">
-          <div className="row contact-row">
-            <div className="formDiv" id="contact">
-              <h3 className="viewTitle" id="contactTitle">
-                HEY THERE, LET'S CONNECT
-              </h3>
-    
-              <hr></hr>
-    
-              <SocialLinks />
-            </div>
-          </div>
-        </div>
-      );
+  return (
+    <div className="viewFive" id="contact">
+      <div className="row contact-row">
+        <div className="formDiv" id="contact">
+          <h3 className="viewTitle" id="contactTitle">
+            GET IN TOUCH
+          </h3>
 
+          <hr></hr>
+
+          <ContactForm />
+          <SocialMedia />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
