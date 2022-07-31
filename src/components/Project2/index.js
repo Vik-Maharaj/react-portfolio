070c2b711ca02b7989ba2c7832563caa1466f012
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-31 19:54:13 -0400

    Add mui project displaying function

--- src/components/Project2/index.js
+++ src/components/Project2/index.js
@@ -79,3 +79,112 @@ const mock = [
     date: '',
   },
 ];
+
+const Project2 = () => {
+  const theme = useTheme();
+  return (
+    <Box>
+  
+      <Grid container spacing={4}>
+        {mock.map((item, i) => (
+          <Grid item xs={12} sm={6} md={4} key={i}>
+            <Box
+              component={'a'}
+              href={item.site} target="_blank"
+              display={'block'}
+              width={1}
+              height={1}
+              sx={{
+                textDecoration: 'none',
+                transition: 'all .2s ease-in-out',
+                '&:hover': {
+                  transform: `translateY(-${theme.spacing(1 / 2)})`,
+                },
+              }}
+            >
+              <Box
+                component={Card}
+                width={1}
+                height={1}
+                boxShadow={4}
+                display={'flex'}
+                flexDirection={'column'}
+                sx={{ backgroundColor: '#f5f5f5' }}
+              >
+                <CardMedia
+                  image={item.image}
+                  title={item.title}
+                  sx={{
+                    height: { xs: 300, md: 360 },
+                    position: 'relative',
+                  }}
+                >
+                  <Box
+                    component={'svg'}
+                    viewBox="0 0 2880 480"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    sx={{
+                      position: 'absolute',
+                      bottom: 0,
+                      color: theme.palette.background.paper,
+                      transform: 'scale(2)',
+                      height: 'auto',
+                      width: 1,
+                      transformOrigin: 'top center',
+                    }}
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M2160 0C1440 240 720 240 720 240H0v240h2880V0h-720z"
+                      fill="#f5f5f5"
+                    />
+                  </Box>
+                </CardMedia>
+                <Box component={CardContent} position={'relative'}>
+                  <Typography variant={'h6'} gutterBottom>
+                    {item.title}
+                  </Typography>
+                  <Typography color="text.secondary">
+                    {item.description}
+                  </Typography>
+                </Box>
+                <Box flexGrow={1} />
+                <Box padding={2} display={'flex'} flexDirection={'column'}>
+                  <Box marginBottom={2}>
+                    <Divider />
+                  </Box>
+                  <Box
+                    display={'flex'}
+                    justifyContent={'space-between'}
+                    alignItems={'center'}
+                  >
+                    <Box display={'flex'} alignItems={'center'}>
+                    <a href={item.author.repo} target="_blank">
+                      <Avatar
+                        src={item.author.avatar}
+                        sx={{ marginRight: 1 }}
+                      />
+                      </a>
+                      <Typography color={'text.secondary'}>
+                        <a href={item.author.repo} target="_blank">
+                        {item.author.repo}
+                        </a>
+                      </Typography>
+                    </Box>
+                    <Typography color={'text.secondary'}>
+                      {item.date}
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default Project2;
