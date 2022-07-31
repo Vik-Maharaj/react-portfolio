baf39bfe2e8fd99ce410b46664f33faf48b2e328
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-31 19:52:47 -0400

    Change folder name and add project mui dependencies

---
+++ src/components/Project2/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import Avatar from '@mui/material/Avatar';
