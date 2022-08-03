4d7cded3ba3b9051fdfceff0ec91987ef67432b3
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-08-03 09:10:23 -0400

    Update projects section index.js

--- src/views/Projects/index.js
+++ src/views/Projects/index.js
@@ -1,18 +1,25 @@
 import React from "react";
 import "./style.css";
-import ProjectList from "../../components/ProjectList";
+import Project2 from "../../components/Project2";
+import Box from '@mui/material/Box';
+import Container from './Container';
 
 function Projects() {
+
     return (
         <div className="viewTwo" id="projects">
             <div className="row">
                 <div className="col-md">
-                    <h3 className="viewTitle" id="projectsTitle">SELECTED PROJECTS</h3>
+                    <h3 className="viewTitle" id="projectsTitle">PROJECTS</h3>
                 </div>
             </div>
-            <div className="row">
-                <ProjectList />
-            </div>
+
+            <Box paddingBottom={{ xs: 2, sm: 3, md: 4 }}>
+                <Container paddingTop={'0 !important'}>
+                    <Project2 />
+                </Container>
+            </Box>
+
         </div>
     )
 }
