d8ed68b4256b0bc44939f108134943227fe1099f
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-08-03 08:53:12 -0400

    Udpate about views index.js

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import MyImage from "../../assets/hero-section-picture_02.png";
+
 import "./style.css";
 
 function About() {
@@ -8,23 +8,24 @@ function About() {
         <div className="viewThree" id="about">
             <div className="row">
                 <div className="col-md about-left">
-                    <img src={MyImage} className="about-photo img-fluid" alt="Me"></img>
                 </div>
                 <div className="col-md">
                     <h3 className="viewTitle" id="aboutTitle">ABOUT</h3>
                     <div className="row align-self-center bioText">
-                        <p className="bio">Here's some more about me:</p>
-                        <p className="bio"><span className="skillHeader">Technical Skills:</span>
+                        <p className="bio">I've previously worked as a videographer, photographer, and graphic designer.</p>
+                        <p className="bio">Now, I'm utilizing my artistic experience and a life-long passion for anything computer-related in a whole new creative realm, <span class="h2-name">Full Stack Web Development!</span></p>
+                        <p className="bio"><span className="skillHeader">Technical skills include:</span>
                             <ul className=" skillList text-center">
                                 <li className="skill">HTML</li>
                                 <li className="skill">CSS</li>
                                 <li className="skill">JavaScript</li>
-                                <li className="skill">Node.js</li>
                                 <li className="skill">React.js</li>
+                                <li className="skill">Node.js</li>
                                 <li className="skill">Express.js</li>
+                                <li className="skill">MySQL</li>
+                                <li className="skill">MongoDB</li>
                             </ul>
                         </p>
-                        <p className="bio">Hit me up!</p>
                     </div>
                 </div>
             </div>
